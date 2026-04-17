822767ef81966d2fc8e28f0103a93f108562c8a3
StefanvonNijf <stefanvannijf@gmail.com>
2026-04-17 12:28:05 +0200

    Update role and Employee Seeders

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -10,8 +10,12 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         DB::table('roles')->insertOrIgnore([
-            ['name' => 'electrician', 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'ironworker', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'electrician',        'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'electrician foreman', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'ironworker',          'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'ironworker foreman',  'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'supervisor',          'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'technician',          'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
